cfe610b29fb6112ce846e72d20a8560d3e40cc53
Takaharu Nakamura <64473501+appare45@users.noreply.github.com>
2023-10-10 10:39:29 +0900

    期限の近さに合わせて点滅速度を変更

--- content.ts
+++ content.ts
@@ -5,3 +5,17 @@ import "./style.scss"
 export const config: PlasmoCSConfig = {
   matches: ["https://manaba.tsukuba.ac.jp/ct/home_library_query"]
 }
+
+document.querySelectorAll(".row0, .row1").forEach((element) => {
+  const period = element.getElementsByClassName("td-period")
+  if (period.length == 2 && !!period[1].textContent) {
+    const endDate = new Date(period[1].textContent)
+    element.setAttribute(
+      "style",
+      `animation-duration:${
+        0.5 + (endDate.valueOf() - Date.now()) / 100000000
+      }s; 
+      `
+    )
+  }
+})
